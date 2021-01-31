de6776d23ec20e0d7ded6b12a05df904d99e05c8
c_diaz8 <carolinaaa123xx@gmail.com>
2021-01-31 11:03:05 -0800

    uploading changes

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"ncundecs
+weather_api_key = "2bb2e4ab14dd70f742dbd3d1d490bee9"
 
 # Google API Key
-g_key = "YOUR KEY HERE!" shendeind
+g_key = "AIzaSyDoB8Jy0F9s1aBmAJpR8rJAoOXnlpTgf24" 
